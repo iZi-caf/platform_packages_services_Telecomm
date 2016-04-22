3bda179a213b991ac14df34c9dcc29123fc09a28
yifan.bai <yifan.bai@spreadtrum.com>
2016-04-22 09:52:24 +0800

    On hold call cannot active when hang up second call with headset media button.
    Reproduce steps:
    1. A phone call is calling with headset.
    2. Another phone call incoming.
    3. Hold the first call and answer the second call.
    4. Hang up the second call with headset media button.
    5. First call should be recovered but still on hold.
    
    Root cause:
    Method Call#disconnect() cannot active on hold call.
    
    Change-Id: If5cdd7e39abe03ccefacac3b4514846bb75d7d54

--- src/com/android/server/telecom/CallsManager.java
+++ src/com/android/server/telecom/CallsManager.java
@@ -1286,7 +1286,7 @@ public class CallsManager extends Call.ListenerBase implements VideoProviderProx
                 Call callToHangup = getFirstCallWithState(
                         CallState.RINGING, CallState.DIALING, CallState.ACTIVE, CallState.ON_HOLD);
                 if (callToHangup != null) {
-                    callToHangup.disconnect();
+                    disconnectCall(callToHangup);
                     return true;
                 }
             }
